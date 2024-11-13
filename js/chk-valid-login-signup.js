c6d52a6b45bc96833e9ca40a5f4c0f24e4fa41c8
Hyeongmin Jo <paengdal1@gmail.com>
2024-11-13 20:47:22 +0900

    fix: 함수내에 삽입한 이벤트 리스너의 제거 코드 추가

--- js/chk-valid-login-signup.js
+++ js/chk-valid-login-signup.js
@@ -2,18 +2,18 @@
 /* 이메일 비밀번호 유효성 체크 */
 
 const USER_DATA = [
-  { email: 'codeit1@codeit.com', password: "codeit101!" },
-  { email: 'codeit2@codeit.com', password: "codeit202!" },
-  { email: 'codeit3@codeit.com', password: "codeit303!" },
-  { email: 'codeit4@codeit.com', password: "codeit404!" },
-  { email: 'codeit5@codeit.com', password: "codeit505!" },
-  { email: 'codeit6@codeit.com', password: "codeit606!" },
-]
+  { email: "codeit1@codeit.com", password: "codeit101!" },
+  { email: "codeit2@codeit.com", password: "codeit202!" },
+  { email: "codeit3@codeit.com", password: "codeit303!" },
+  { email: "codeit4@codeit.com", password: "codeit404!" },
+  { email: "codeit5@codeit.com", password: "codeit505!" },
+  { email: "codeit6@codeit.com", password: "codeit606!" },
+];
 
-const form = document.querySelector('form');
-const button = document.querySelectorAll('button')[1];
-let inputEmailValue = '';
-let inputPwValue = '';
+const form = document.querySelector("form");
+const button = document.querySelectorAll("button")[1];
+let inputEmailValue = "";
+let inputPwValue = "";
 
 let isEmailChecked = false;
 let isPwChecked = false;
@@ -22,7 +22,7 @@ let isPwConfirmChecked = false;
 
 // 에러 메시지 만들기
 function createMsgDiv(msgTxt) {
-  const msgDiv = document.createElement('div');
+  const msgDiv = document.createElement("div");
   msgDiv.classList.add("error-msg");
   msgDiv.textContent = msgTxt;
   return msgDiv;
@@ -31,14 +31,17 @@ function createMsgDiv(msgTxt) {
 // 이메일 형식 체크
 const PATTERN = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-za-z0-9\-]+/;
 function emailValidChk(email) {
-  if (PATTERN.test(email) === false) { return false; }
-  else { return true; }
+  if (PATTERN.test(email) === false) {
+    return false;
+  } else {
+    return true;
+  }
 }
 
 // 유효성 체크 핸들러
 function validCheck(e) {
   const { target } = e;
-  const inputId = target.getAttribute('id'); // input 종류 받아오기
+  const inputId = target.getAttribute("id"); // input 종류 받아오기
   const inputValue = target.value; // input 입력값
 
   // 에러 효과 적용
@@ -50,7 +53,8 @@ function validCheck(e) {
   // 에러 효과 삭제
   function removeErrStyle() {
     const next = target.nextElementSibling;
-    if (next && next.classList.contains("error-msg")) { // 에러 메시지가 있는 경우 삭제
+    if (next && next.classList.contains("error-msg")) {
+      // 에러 메시지가 있는 경우 삭제
       target.nextElementSibling.remove();
     }
     target.classList.remove("error-input"); // input 에러 스타일 삭제
@@ -58,56 +62,75 @@ function validCheck(e) {
 
   // 버튼 활성화 및 이벤트 핸들링
   function activeBtn() {
-    button.classList.add('active-button');
-    button.addEventListener('click', checkUserData);
+    button.classList.add("active-button");
+    button.removeEventListener("click", checkUserData);
+    button.addEventListener("click", checkUserData);
   }
 
-  if (inputValue) { // 1. 값 입력 시
-    if (inputId === 'email') { // 1-1. 이메일 입력의 경우
-      if (emailValidChk(inputValue)) { // 1-1-1. 이메일이 유효하면
+  if (inputValue) {
+    // 1. 값 입력 시
+    if (inputId === "email") {
+      // 1-1. 이메일 입력의 경우
+      if (emailValidChk(inputValue)) {
+        // 1-1-1. 이메일이 유효하면
         removeErrStyle();
         inputEmailValue = inputValue;
         isEmailChecked = true;
-      } else { // 1-1-2. 이메일이 유효하지 않으면
+      } else {
+        // 1-1-2. 이메일이 유효하지 않으면
         removeErrStyle();
         setErrStyle("잘못된 이메일 형식입니다.");
         return false;
       }
-    } else if (inputId === 'password') { // 1-2. 비밀번호 입력의 경우
-      if (inputValue.length < 8) { // 1-2-1. 비밀번호가 8자리 미만이면
+    } else if (inputId === "password") {
+      // 1-2. 비밀번호 입력의 경우
+      if (inputValue.length < 8) {
+        // 1-2-1. 비밀번호가 8자리 미만이면
         removeErrStyle();
         setErrStyle("비밀번호를 8자 이상 입력해주세요.");
         return false;
-      } else { // 1-2-2. 비밀번호가 8자리 이상이면
+      } else {
+        // 1-2-2. 비밀번호가 8자리 이상이면
         removeErrStyle();
         inputPwValue = inputValue;
         isPwChecked = true;
       }
-    } else if (inputId === 'password-confirm') { // 1-3. 비밀번호 확인의 경우
-      if (inputPwValue !== inputValue) { // 1-3-1. 비밀번호가 일치하지 않으면
+    } else if (inputId === "password-confirm") {
+      // 1-3. 비밀번호 확인의 경우
+      if (inputPwValue !== inputValue) {
+        // 1-3-1. 비밀번호가 일치하지 않으면
         removeErrStyle();
         setErrStyle("비밀번호가 일치하지 않습니다.");
         return false;
-      } else { // 1-3-2. 비밀번호가 일치하면
+      } else {
+        // 1-3-2. 비밀번호가 일치하면
         removeErrStyle();
         isPwConfirmChecked = true;
       }
-    } else { // 1-4. 닉네임 입력의 경우
-      if (inputValue) { // 1-4-1. 닉네임을 입력했으면
+    } else {
+      // 1-4. 닉네임 입력의 경우
+      if (inputValue) {
+        // 1-4-1. 닉네임을 입력했으면
         removeErrStyle();
         isNickNameChecked = true;
-      } else { // 1-4-2. 닉네임을 입력하지 않았으면
+      } else {
+        // 1-4-2. 닉네임을 입력하지 않았으면
         setErrStyle("닉네임을 입력해주세요.");
         return false;
       }
     }
-
-  } else { // 2. 값 미입력 시
+  } else {
+    // 2. 값 미입력 시
     removeErrStyle(); // 기존 에러 메시지 제거
-    if (!target.classList.contains('error-input') && target.getAttribute('type') !== 'button') { // 에러 메시지 중복 생성 방지
-      const msg = (inputId === 'email')
-        ? "이메일을 입력해주세요."
-        : (inputId === 'nickname')
+    if (
+      !target.classList.contains("error-input") &&
+      target.getAttribute("type") !== "button"
+    ) {
+      // 에러 메시지 중복 생성 방지
+      const msg =
+        inputId === "email"
+          ? "이메일을 입력해주세요."
+          : inputId === "nickname"
           ? "닉네임을 입력해주세요"
           : "비밀번호를 입력해주세요.";
       setErrStyle(msg);
@@ -115,12 +138,19 @@ function validCheck(e) {
   }
 
   // 유효성 체크 완료 시 버튼 활성화
-  if (button.className === 'login') { // 1. 로그인 버튼
+  if (button.className === "login") {
+    // 1. 로그인 버튼
     if (isEmailChecked && isPwChecked) {
       activeBtn();
     }
-  } else { // 2. 회원가입 버튼
-    if (isEmailChecked && isPwChecked && isPwConfirmChecked && isNickNameChecked) {
+  } else {
+    // 2. 회원가입 버튼
+    if (
+      isEmailChecked &&
+      isPwChecked &&
+      isPwConfirmChecked &&
+      isNickNameChecked
+    ) {
       activeBtn();
     }
   }
@@ -129,28 +159,36 @@ function validCheck(e) {
 // 로그인/회원가입 버튼 클릭 시 데이터 체크 핸들러
 function checkUserData(e) {
   const { target } = e;
-  const modal = document.querySelector('.modal');
-  const closeBtn = document.querySelector('.close-btn');
+  const modal = document.querySelector(".modal");
+  const closeBtn = document.querySelector(".close-btn");
 
+  function removeModal() {
+    modal.classList.remove("on");
+  }
   // 모달 팝업, 닫기 버튼 이벤트 핸들링
   function popModal() {
-    modal.classList.add('on');
-    closeBtn.addEventListener('click', () => { modal.classList.remove('on'); });
+    modal.classList.add("on");
+    closeBtn.removeEventListener("click", removeModal);
+    closeBtn.addEventListener("click", removeModal);
   }
 
-  if (target.classList.contains('login')) { // 1. 로그인
-    let userExist = USER_DATA.some((el) => el.email === inputEmailValue && el.password === inputPwValue);
+  if (target.classList.contains("login")) {
+    // 1. 로그인
+    let userExist = USER_DATA.some(
+      (el) => el.email === inputEmailValue && el.password === inputPwValue
+    );
     if (userExist) {
-      window.location.href = '/items';
+      window.location.href = "/items";
     } else {
       popModal();
     }
-  } else { // 2. 회원가입
+  } else {
+    // 2. 회원가입
     let emailExist = USER_DATA.some((el) => el.email === inputEmailValue);
     if (emailExist) {
       popModal();
     } else {
-      window.location.href = '/login';
+      window.location.href = "/login";
     }
   }
 }
